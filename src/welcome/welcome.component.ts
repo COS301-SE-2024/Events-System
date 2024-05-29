e7f504a0ffe2cff32c8a25203ab51ecc9223ee73
Ofentse Ramothibe <u21467422@tuks.co.za>
2024-05-30 01:49:33 +0200

    added mock data for welcome page

--- src/welcome/welcome.component.ts
+++ src/welcome/welcome.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { featherAirplay } from '@ng-icons/feather-icons';
+import { heroUsers } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgIconComponent],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css',
+  viewProviders: [provideIcons({ featherAirplay, heroUsers })]
 })
 
 export class WelcomeComponent {}
