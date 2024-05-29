e8f91de32f1832676848e0aa8cfce9cb2b7a593b
Mathenda <mathendam@gmail.com>
2024-05-29 10:12:26 +0200

    added health check for backend

---
+++ Backend/src/main/java/com/back/demo/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.back.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/healthcheck")
+    public String healthCheck() {
+        return "Working";
+    }
+}
