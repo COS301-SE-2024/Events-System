6c56e9e1afa7c448558791b8186b0c358dc0b140
Mathenda <mathendam@gmail.com>
2024-05-29 20:48:05 +0200

    Revert "added backend and frontend standards and NX guide"
    This reverts commit ea417a5cc2553d1344bfd03c4fa8018e1b38d0f9.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,11 @@ import { Route } from '@angular/router';
 import { LoginComponent } from 'src/login/login.component';
 import { SettingsComponent } from 'src/settings/settings.component';
 import { HomeComponent } from 'src/home/home.component';
-import { PorfolioComponent } from 'src/portfolio/porfolio.component';
-
+import { AppComponent } from './app.component';
 
 export const appRoutes: Route[] = [
   { path: '', component: HomeComponent }, // Home page
   { path: 'login', component: LoginComponent },
   { path: 'settings', component: SettingsComponent },
-   { path: 'portfolio', component: PorfolioComponent },
+  // { path: 'home', component: SettingsComponent },
 ];
