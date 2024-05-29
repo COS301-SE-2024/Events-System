a7670332189c515e207c9af33f7c4e090e5b43e2
Ofentse Ramothibe <u21467422@tuks.co.za>
2024-05-29 23:26:08 +0200

    Added palceholder UI for welcome page

---
+++ src/welcome/welcome.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.css',
+})
+
+export class WelcomeComponent {}
