78c801c39c29fe735a39eb86cfa9d9b17c8adb72
Mathenda <mathendam@gmail.com>
2024-05-30 14:33:12 +0200

    added better backend home page app

--- Backend/src/main/java/com/back/demo/BackendApplication.java
+++ Backend/src/main/java/com/back/demo/BackendApplication.java
@@ -2,9 +2,17 @@ package com.back.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
 public class BackendApplication {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
