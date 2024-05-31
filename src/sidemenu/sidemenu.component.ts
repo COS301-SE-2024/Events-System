45b27f0ed26452c53deb48bb57814a52862eba65
Ofentse Ramothibe <u21467422@tuks.co.za>
2024-06-01 01:01:09 +0200

    Created sidemenu component

---
+++ src/sidemenu/sidemenu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sidemenu',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sidemenu.component.html',
+  styleUrl: './sidemenu.component.css',
+})
+export class SidemenuComponent {}
