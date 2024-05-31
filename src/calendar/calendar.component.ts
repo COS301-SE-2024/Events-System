b13a3bf9600afe1a05cc50876fa86a46201e1dac
Ofentse Ramothibe <u21467422@tuks.co.za>
2024-05-31 14:32:26 +0200

    Added calendar component

---
+++ src/calendar/calendar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-calendar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './calendar.component.html',
+  styleUrl: './calendar.component.css',
+})
+export class CalendarComponent {}
