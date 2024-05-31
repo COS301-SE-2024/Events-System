4d8647c83b8ba3e20cc8861ef9f712401b525900
Ofentse Ramothibe <u21467422@tuks.co.za>
2024-06-01 01:01:37 +0200

    Linked navbar to home page

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from 'src/navbar/navbar.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
