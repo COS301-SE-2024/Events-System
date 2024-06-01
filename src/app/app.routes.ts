f75a4774495a53edd822158075eb5cde6066ecb8
Ofentse Ramothibe <99901171+ofentse-ramothibe@users.noreply.github.com>
2024-06-01 17:02:02 +0200

    added calender component and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from 'src/login/login.component';
 import { SettingsComponent } from 'src/settings/settings.component';
 import { HomeComponent } from 'src/home/home.component';
 import { AppComponent } from './app.component';
+import { CalenderComponent } from 'src/calender/calender.component';
 
 export const appRoutes: Route[] = [
   { path: '', component: HomeComponent }, // Home page
   { path: 'login', component: LoginComponent },
   { path: 'settings', component: SettingsComponent },
+  { path: 'calender', component: CalenderComponent },
   // { path: 'home', component: SettingsComponent },
 ];
---
+++ src/calender/calender.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-calender',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './calender.component.html',
+  styleUrl: './calender.component.css',
+})
+export class CalenderComponent {}
