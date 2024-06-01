94bb4656f7effe33dde02e5fa74a5874140cea9e
Ofentse Ramothibe <99901171+ofentse-ramothibe@users.noreply.github.com>
2024-06-01 19:13:18 +0200

    added dynamic calender view

--- src/calender/calender.component.ts
+++ src/calender/calender.component.ts
@@ -8,4 +8,29 @@ import { CommonModule } from '@angular/common';
   templateUrl: './calender.component.html',
   styleUrl: './calender.component.css',
 })
-export class CalenderComponent {}
+export class CalenderComponent {
+  currentDate = new Date();
+
+
+  getDaysInMonth() {
+    const daysInMonth = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() + 1, 0).getDate();
+    const days = Array.from({length: daysInMonth}, (_, i) => i + 1);
+    const weeks = [];
+    while(days.length) {
+      weeks.push(days.splice(0, 7));
+    }
+    return weeks;
+  }
+
+
+  nextMonth() {
+    this.currentDate.setMonth(this.currentDate.getMonth() + 1);
+  }
+
+
+  previousMonth() {
+    this.currentDate.setMonth(this.currentDate.getMonth() - 1);
+  }
+
+
+}
