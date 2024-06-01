4b35622d72cc39a461cf3a4b18dddbde612a64e3
Ofentse Ramothibe <99901171+ofentse-ramothibe@users.noreply.github.com>
2024-06-01 19:48:18 +0200

    adjusted wolcome ts

--- src/welcome/welcome.component.ts
+++ src/welcome/welcome.component.ts
@@ -2,18 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
-import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { featherAirplay } from '@ng-icons/feather-icons';
-import { heroUsers } from '@ng-icons/heroicons/outline';
+// import { NgIconComponent, provideIcons } from '@ng-icons/core';
+// import { featherAirplay } from '@ng-icons/feather-icons';
+// import { heroUsers } from '@ng-icons/heroicons/outline';
 import { LoginComponent } from 'src/login/login.component';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [CommonModule, RouterModule, NgIconComponent, LoginComponent],
+  imports: [CommonModule, RouterModule, LoginComponent],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css',
-  viewProviders: [provideIcons({ featherAirplay, heroUsers })]
+  viewProviders: []
 })
 
 export class WelcomeComponent {}
