c6dd447131543aba53dafbf8ad18568d00bd8fa6
Ofentse Ramothibe <99901171+ofentse-ramothibe@users.noreply.github.com>
2024-06-01 22:35:53 +0200

    fixed failing tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { WelcomeComponent } from 'src/welcome/welcome.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, WelcomeComponent],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { NavbarComponent } from 'src/navbar/navbar.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, NavbarComponent],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
