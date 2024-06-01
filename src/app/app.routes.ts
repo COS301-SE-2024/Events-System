fe89c52198d78c1d152f0cdcc97f0bb1a50a4be9
Ofentse Ramothibe <99901171+ofentse-ramothibe@users.noreply.github.com>
2024-06-01 22:25:00 +0200

    added profile page and link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Route } from '@angular/router';
 import { LoginComponent } from 'src/login/login.component';
 import { SettingsComponent } from 'src/settings/settings.component';
 import { HomeComponent } from 'src/home/home.component';
+import { ProfileComponent } from 'src/profile/profile.component';
 import { AppComponent } from './app.component';
 import { CalenderComponent } from 'src/calender/calender.component';
 
@@ -10,5 +11,6 @@ export const appRoutes: Route[] = [
   { path: 'login', component: LoginComponent },
   { path: 'settings', component: SettingsComponent },
   { path: 'calender', component: CalenderComponent },
+  { path: 'profile', component: ProfileComponent },
   // { path: 'home', component: SettingsComponent },
 ];
