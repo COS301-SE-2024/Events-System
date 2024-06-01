087a501d48d7e0b5f24dc3531245683f2647e508
Ofentse Ramothibe <99901171+ofentse-ramothibe@users.noreply.github.com>
2024-06-01 22:29:41 +0200

    removed redundancies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,11 @@ import { Route } from '@angular/router';
 import { LoginComponent } from 'src/login/login.component';
 import { SettingsComponent } from 'src/settings/settings.component';
 import { HomeComponent } from 'src/home/home.component';
-import { WelcomeComponent } from 'src/welcome/welcome.component';
-import { CalendarInlineDemo } from 'src/calendar/calendar.component';
 import { AppComponent } from './app.component';
 
 export const appRoutes: Route[] = [
   { path: '', component: HomeComponent }, // Home page
   { path: 'login', component: LoginComponent },
   { path: 'settings', component: SettingsComponent },
-  { path: 'welcome', component: WelcomeComponent },
-  { path: 'calendar', component: CalendarInlineDemo },
   // { path: 'home', component: SettingsComponent },
 ];
