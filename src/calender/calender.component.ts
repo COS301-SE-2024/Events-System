916b87fce72a521946b3ce8b6d76bd172e0319a9
Ofentse Ramothibe <99901171+ofentse-ramothibe@users.noreply.github.com>
2024-06-01 21:26:28 +0200

    adjusted view and added event component

--- src/calender/calender.component.ts
+++ src/calender/calender.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {CalenderEventButtonComponent} from "src/Components/Calender-Event-Button/Calender-Event-button.component";
 
 @Component({
   selector: 'app-calender',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CalenderEventButtonComponent],
   templateUrl: './calender.component.html',
   styleUrls: ['./calender.component.css'],
 })
@@ -32,4 +32,24 @@ export class CalenderComponent {
   previousMonth() {
     this.currentDate = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() - 1, 1);
   }
+  getCalendarHTML() {
+    const weeks = this.getDaysInMonth();
+    let html = '<table><thead><tr><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th><th>Sun</th></tr></thead><tbody>';
+  
+  
+    for (const week of weeks) {
+      html += '<tr>';
+      for (const day of week) {
+        html += `<td>${day || ''}</td>`;
+      }
+      html += '</tr>';
+    }
+  
+  
+    html += '</tbody></table>';
+  
+  
+    return html;
+  }
+  
 }
