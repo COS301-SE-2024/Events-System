a4fb1f640b616dee5086f825cc85eecba7a39bdc
KezlynGovender <u22567993@tuks.co.za>
2024-06-02 12:20:56 +0200

    Created profile page structure

---
+++ src/Components/userProfileComponent/userProfile.componenet.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UserProfileComponent } from './userProfile.component';
+
+describe('UserProfileComponent', () => {
+  let component: UserProfileComponent;
+  let fixture: ComponentFixture<UserProfileComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UserProfileComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(UserProfileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Route } from '@angular/router';
 import { LoginComponent } from 'src/login/login.component';
 import { SettingsComponent } from 'src/settings/settings.component';
 import { HomeComponent } from 'src/home/home.component';
+import { UserProfileComponent } from 'src/Components/userProfileComponent/userProfile.component';
 import { AppComponent } from './app.component';
 
 export const appRoutes: Route[] = [
   { path: '', component: HomeComponent }, // Home page
   { path: 'login', component: LoginComponent },
   { path: 'settings', component: SettingsComponent },
+  { path: 'userProfile', component: UserProfileComponent },
   // { path: 'home', component: SettingsComponent },
 ];
