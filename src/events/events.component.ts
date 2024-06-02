01f6b2ef2db6808c5970fbe823f95673e48b7a54
Ofentse Ramothibe <99901171+ofentse-ramothibe@users.noreply.github.com>
2024-06-02 13:33:55 +0200

    added dynamic events

--- src/events/events.component.ts
+++ src/events/events.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventComponent } from 'src/Components/event/event.component';
+import { response } from 'express';
 @Component({
   selector: 'app-events',
   standalone: true,
@@ -8,4 +9,22 @@ import { EventComponent } from 'src/Components/event/event.component';
   templateUrl: './events.component.html',
   styleUrl: './events.component.css',
 })
-export class EventsComponent {}
+export class EventsComponent implements OnInit {
+  events: any[] = [];
+
+
+  ngOnInit(): void {
+    fetch('/api/events')
+      .then(response => {
+        console.log(response); // This will log the raw response object
+        return response.json();
+      })
+      .then(data => {
+        console.log(data); // This will log the parsed JSON data
+        this.events = Array.isArray(data) ? data : [data];
+      });
+  }
+  
+    
+
+}
