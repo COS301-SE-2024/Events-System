076d6c83505ac44b108ea98d9c1ed12a3a10059b
Mathenda <mathendam@gmail.com>
2024-06-02 19:53:19 +0200

    removed secrets

--- Backend/src/main/java/com/back/demo/BackendApplication.java
+++ Backend/src/main/java/com/back/demo/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.back.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
