76f4e043fb55961d3210e412d1ffdc572efb48a4
Ofentse Ramothibe <99901171+ofentse-ramothibe@users.noreply.github.com>
2024-06-02 16:45:46 +0200

    finalized events page

--- src/events/events.component.ts
+++ src/events/events.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventComponent } from 'src/Components/event/event.component';
-import { FormControl, Validators } from '@angular/forms';
 import { response } from 'express';
-
-
 @Component({
   selector: 'app-events',
   standalone: true,
@@ -12,11 +9,8 @@ import { response } from 'express';
   templateUrl: './events.component.html',
   styleUrl: './events.component.css',
 })
-
-
 export class EventsComponent implements OnInit {
   events: any[] = [];
-  eventCreated = false;
 
 
   event = {
@@ -30,84 +24,32 @@ export class EventsComponent implements OnInit {
     socialClub: ''
   };
   onSubmit(event: Event): void {
-    event.preventDefault();
+    event.preventDefault(); // Prevent the form from refreshing the page
 
 
     const form = event.target as HTMLFormElement;
     const formData = new FormData(form);
- 
-    let title = formData.get('title') as string;
-    let description = formData.get('description') as string;
-    let startDate = formData.get('startDate') as string;
-    let startTime = formData.get('startTime') as string;
-    let endDate = formData.get('endDate') as string;
-    let endTime = formData.get('endTime') as string;
-  
-    let location = formData.get('location') as string;
-    let socialClub = formData.get('socialClub') as string;
-
-      // Combine date and time into a single string
-    let startDateTime = `${startDate}T${startTime}:00Z`;
-    let endDateTime = `${endDate}T${endTime}:00Z`;
-
-    // Convert to Date objects
-    let start = new Date(startDateTime);
-    let end = new Date(endDateTime);
 
 
-    // Check if end time is before start time
-    if (end < start) {
-      alert('End time cannot be before start time');
-      return;
-    }
-
-    // Sanitize the form values
-    title = this.sanitizeInput(title);
-    description = this.sanitizeInput(description);
-    // Sanitize the date-time strings
-    startDateTime = this.sanitizeInput(startDateTime);
-    endDateTime = this.sanitizeInput(endDateTime);
-
-    location = this.sanitizeInput(location);
-    socialClub = this.sanitizeInput(socialClub);
-    // Check each form value
-    if (!title) {
-      alert('Title is required.');
-      return;
-    }
-    if (!description) {
-      alert('Description is required.');
-      return;
-    }
-    if (!location) {
-      alert('Location is required.');
-      return;
-    }
-    if (!socialClub) {
-      alert('Social club is required.');
-      return;
-    }
- 
     const eventDetails = {
-      title,
-      description,
-      startDateTime,
-      endDateTime,
-      location,
+      title: formData.get('title'),
+      description: formData.get('description'),
+      startTime: formData.get('startTime'),
+      endTime: formData.get('endTime'),
+      location: formData.get('location'),
       hostId: 1,
       geolocation: "new Geo",
-      socialClub,
+      socialClub: formData.get('socialClub'),
+      // Add other fields as necessary
     };
- 
+
+
     this.createEvent(eventDetails);
   }
-  sanitizeInput(input: string): string {
-    // Remove any script tags
-    const sanitizedInput = input.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
-    return sanitizedInput;
-  }
+
+
   createEvent(event: any): void {
-    fetch('/api/events', {
+    fetch('https://events-system-back.wn.r.appspot.com/api/events', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -117,11 +59,6 @@ export class EventsComponent implements OnInit {
     .then(response => response.json())
     .then(data => {
       console.log(data); // Log the response data for debugging
-      this.eventCreated = true; // Set eventCreated to true
-      // Hide the toast after 5 seconds
-      setTimeout(() => {
-        this.hideToast();
-      }, 5000);
     })
     .catch(error => {
       console.error('Error:', error);
@@ -130,7 +67,7 @@ export class EventsComponent implements OnInit {
 
 
   ngOnInit(): void {
-    fetch('/api/events')
+    fetch('https://events-system-back.wn.r.appspot.com/api/events')
       .then(response => {
         return response.json();
       })
@@ -139,12 +76,8 @@ export class EventsComponent implements OnInit {
       });
   }
  
-  hideToast(): void {
-    this.eventCreated = false;
-  }
+
+
 
 
 }
-
-
-
