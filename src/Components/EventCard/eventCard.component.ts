95407d0c80809574397d472d398bf6b6ca8a5632
Ofentse Ramothibe <u21467422@tuks.co.za>
2024-06-09 16:17:06 +0200

    Created event card component

---
+++ src/Components/EventCard/eventCard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-event-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './eventCard.component.html',
+  styleUrl: './eventCard.component.css',
+})
+export class EventCardComponent {}
