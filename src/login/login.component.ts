aa5252a072d7f0a2aeaba0706132a68f8860e69a
SageBoots <u20456213@tuks.co.za>
2024-06-15 07:44:44 +0200

    Began integration of signup

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -1,22 +1,62 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms'; 
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule, RouterModule], // Import necessary modules here
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  registerForm: FormGroup;
 
-  onSubmit() {
-    // Perform login logic here
-    // ...
+  constructor(
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      role: ['USER', Validators.required] // Default role is Employee
+    });
+  }
 
-    // Navigate to the desired page after successful login
-    this.router.navigate(['']);
+  onRegister(event: Event) {
+    event.preventDefault();
+
+    if (this.registerForm.valid) {
+      const formData = {
+      firstName: this.registerForm.get('firstName')?.value,
+      lastName: this.registerForm.get('lastName')?.value,
+      email: this.registerForm.get('email')?.value,
+      password: this.registerForm.get('password')?.value
+    };
+
+      // Assuming API endpoint for registration
+      fetch('https://events-system-back.wn.r.appspot.com/api/v1/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Registration successful:', data);
+        // Optionally, navigate to another page on successful registration
+        this.router.navigate(['']);
+      })
+      .catch(error => {
+        console.error('Error registering:', error);
+      });
+    } else {
+      console.log('Form is invalid. Please check the fields.');
+    }
   }
 }
