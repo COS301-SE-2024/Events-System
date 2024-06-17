9a95707420dbdc0d8d8e7b4e79f90111e82f030b
KezlynGovender <u22567993@tuks.co.za>
2024-06-18 00:55:50 +0200

    Added some default functions

--- src/settings/settings.component.ts
+++ src/settings/settings.component.ts
@@ -8,4 +8,31 @@ import { RouterModule } from '@angular/router';
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css',
 })
-export class SettingsComponent {}
+export class SettingsComponent {
+  selectedTab: string = 'details';
+  name: string = '';
+  surname: string = '';
+  description: string = '';
+  email: string = '';
+  x: string = '';
+  linkedIn: string = '';
+  gitHub: string = '';
+  currentPassword: string = '';
+  newPassword: string = '';
+  confirmPassword: string = '';
+  
+  makeContactInfoPublic: boolean = false;
+  makeScreenerPublic: boolean = false;
+
+  saveChanges(): void {
+    console.log('Changes saved');
+  }
+
+  deleteAccount(): void {
+    console.log('Account deleted');
+  }
+
+  selectTab(tab: string): void {
+    this.selectedTab = tab;
+  }
+}
