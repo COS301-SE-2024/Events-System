8ad416932bf0d3313b2aa4ec85b265d04cda387b
Mathenda <mathendam@gmail.com>
2024-06-19 01:05:27 +0200

    added skeletons

--- src/MyEvents/MyEvents.component.ts
+++ src/MyEvents/MyEvents.component.ts
@@ -2,25 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MyEventsCardComponent } from 'src/Components/MyEventsCard/MyEventsCard.component';
+import { MyEventsCardSkeletonComponent } from 'src/Components/MyEventsCardSkeleton/MyEventsCardSkeleton.component';
 
 
 @Component({
   selector: 'app-my-events',
   standalone: true,
-  imports: [CommonModule, RouterModule, MyEventsCardComponent],
+  imports: [CommonModule, RouterModule, MyEventsCardComponent, MyEventsCardSkeletonComponent],
   templateUrl: './MyEvents.component.html',
   styleUrl: './MyEvents.component.css',
 })
 export class MyEventsComponent implements OnInit {
+  isLoading = true;
   events: any[] = [];
   ngOnInit(): void {
+    this.isLoading = true;
     fetch('https://events-system-back.wn.r.appspot.com/api/events')
       .then(response => {
         return response.json();
       })
       .then(data => {
         this.events = Array.isArray(data) ? data : [data];
-        console.log(this.events);
+        this.isLoading = false;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        this.isLoading = false;
       });
   }
 }
