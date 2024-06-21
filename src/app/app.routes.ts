e04ae735182303a9fdfb52a2d769b342d1ce2f25
Ofentse Ramothibe <99901171+ofentse-ramothibe@users.noreply.github.com>
2024-06-21 21:30:44 +0200

    added social club page and dynamic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { ProfileComponent } from 'src/profile/profile.component';
 import { AppComponent } from './app.component';
 import { CalenderComponent } from 'src/calender/calender.component';
 import { EventsComponent } from 'src/events/events.component';
-
+import { SocialClubComponent } from 'src/socialClub/socialClub.component';
 export const appRoutes: Route[] = [
   { path: '', component: HomeComponent }, // Home page
   { path: 'login', component: LoginComponent },
@@ -14,5 +14,7 @@ export const appRoutes: Route[] = [
   { path: 'calender', component: CalenderComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'events', component: EventsComponent },
+  { path: 'socialclub/:id', component: SocialClubComponent}
+
   // { path: 'home', component: SettingsComponent },
 ];
---
+++ src/socialClub/socialClub.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-social-club',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './socialClub.component.html',
+  styleUrl: './socialClub.component.css',
+})
+export class SocialClubComponent {}
