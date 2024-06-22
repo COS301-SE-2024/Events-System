ec1300a371d5a6e6d098ff31c3c989f7d9851e0c
Mathenda <mathendam@gmail.com>
2024-06-22 13:34:02 +0200

    removed animations

--- src/Help/Help.component.ts
+++ src/Help/Help.component.ts
@@ -1,21 +1,12 @@
 import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { trigger, transition, style, animate } from '@angular/animations';
-
 @Component({
   selector: 'app-help',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './Help.component.html',
   styleUrl: './Help.component.css',
-  animations: [
-    trigger('fadeInOut', [
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('400ms', style({ opacity: 1 }))
-      ])
-    ])]
 })
 export class HelpComponent {
   activeSection = 'gettingStarted'; // Default active section
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
 @Component({
   standalone: true,
-  imports: [RouterModule, BrowserModule, BrowserAnimationsModule],
+  imports: [RouterModule, CommonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
