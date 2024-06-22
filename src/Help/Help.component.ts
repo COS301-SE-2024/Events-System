ac85697a148022553d9a018f047047fcdfb6e8fc
Mathenda <mathendam@gmail.com>
2024-06-22 13:28:04 +0200

    added mobile viewport

--- src/Help/Help.component.ts
+++ src/Help/Help.component.ts
@@ -1,18 +1,30 @@
 import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-help',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './Help.component.html',
   styleUrl: './Help.component.css',
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('400ms', style({ opacity: 1 }))
+      ])
+    ])]
 })
 export class HelpComponent {
   activeSection = 'gettingStarted'; // Default active section
+  selectedSection = 'gettingStarted'; // Initialize selectedSection
 
   // Function to change the active section
   setActiveSection(section: string) {
     this.activeSection = section;
+    this.selectedSection = section; // Ensure both activeSection and selectedSection are updated
+    // Additional logic to handle section change
   }
 }
