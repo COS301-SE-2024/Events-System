426920729cb1b92e7c97d33ba6f1ca1e97f8a017
Mathenda <mathendam@gmail.com>
2024-06-22 13:31:11 +0200

    added browser animation import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, BrowserModule, BrowserAnimationsModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
