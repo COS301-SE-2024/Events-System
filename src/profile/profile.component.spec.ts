a7240f3ebffe4de5d52c8d7aaa0fa1eefa4ed3ad
KezlynGovender <u22567993@tuks.co.za>
2024-06-24 00:28:46 +0200

    Test that profile name and username is displayed

--- src/profile/profile.component.spec.ts
+++ src/profile/profile.component.spec.ts
@@ -22,6 +22,15 @@ describe('ProfileComponent', () => {
     expect(ProfileComponent).toBeTruthy();
   });
 
+ 
+  it('should display the profile name and username', async () => {
+    const profileNames = await screen.findAllByText(/profile name/i);
+    const profileUsernames = await screen.findAllByText(/profile username/i);
+    expect(profileNames[0]).toBeInTheDocument();
+    expect(profileUsernames[0]).toBeInTheDocument();
+  });
+  
+
   it('should have an Edit Profile button', async () => {
     const editProfileButtons = await screen.findAllByRole('button', { name: /edit profile/i });
     expect(editProfileButtons[0]).toBeInTheDocument(); // Use the first match or identify the specific element based on its position
