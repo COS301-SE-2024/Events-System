9926076183254da8a09439fe0bbb071d8764c903
Mathenda <mathendam@gmail.com>
2024-06-24 15:49:34 +0200

    adjusted login

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -106,7 +106,11 @@ export class LoginComponent {
 
         // Fetch employee data using ID
         const employeeId = localStorage.getItem('ID');
-        if (!employeeId) {
+        if (employeeId) {
+          const employeeResponse = await this.http.get(`https://events-system-back.wn.r.appspot.com/api/employees/${employeeId}`).toPromise();
+          localStorage.setItem('employeeData', JSON.stringify(employeeResponse));
+          console.log('Employee data:', localStorage.getItem('employeeData'));
+        } else {
           console.warn('No ID found in localStorage');
         }
 
