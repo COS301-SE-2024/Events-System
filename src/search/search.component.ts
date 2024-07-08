210b80bbe700dcf7c6499a6ab1c4104805f3421f
Ofentse Ramothibe <u21467422@tuks.co.za>
2024-07-08 07:56:21 +0100

    Created search page and linked to app

---
+++ src/search/search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css',
+})
+export class SearchComponent {}
