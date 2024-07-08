dc9a6bc861b647306b48234ef19fc8e13f7859a0
Ofentse Ramothibe <u21467422@tuks.co.za>
2024-07-08 17:26:14 +0100

    Added card to search page

--- src/search/search.component.ts
+++ src/search/search.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SearchEventCardComponent} from 'src/Components/SearchEventCard/searchEventCard.component';
+import {SearchHostCardComponent } from 'src/Components/searchHostCard/searchHostCard.component';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [CommonModule, SearchEventCardComponent],
+  imports: [CommonModule, SearchEventCardComponent, SearchHostCardComponent],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css',
 })
