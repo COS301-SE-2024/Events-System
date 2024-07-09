3430a6796f519d79a1120c4fe2cb3142ee20ee7a
Ofentse Ramothibe <u21467422@tuks.co.za>
2024-07-09 14:03:21 +0100

    Added search social club to search host card

--- src/Components/searchHostCard/searchHostCard.component.ts
+++ src/Components/searchHostCard/searchHostCard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SearchSocialClubCardComponent } from 'src/Components/SearchSocialClubCard/searchSocialClubCard.component';
 
 @Component({
   selector: 'app-search-host-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SearchSocialClubCardComponent],
   templateUrl: './searchHostCard.component.html',
   styleUrl: './searchHostCard.component.css',
 })
