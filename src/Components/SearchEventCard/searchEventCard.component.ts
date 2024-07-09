a36a6e6d266fc2ca92ec089d65feb9d5c9810b01
Ofentse Ramothibe <u21467422@tuks.co.za>
2024-07-09 18:19:50 +0100

    Added tag component to search event card

--- src/Components/SearchEventCard/searchEventCard.component.ts
+++ src/Components/SearchEventCard/searchEventCard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TagComponent } from 'src/Components/SearchTag/tag.component';
 
 @Component({
   selector: 'app-search-event-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TagComponent],
   templateUrl: './searchEventCard.component.html',
   styleUrl: './searchEventCard.component.css',
 })
