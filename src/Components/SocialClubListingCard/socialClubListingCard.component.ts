e8c61b7ce310ee1139ae97eff894c7c1dd660eaa
Ofentse Ramothibe <u21467422@tuks.co.za>
2024-07-15 19:37:09 +0200

    Fixed routing

--- src/Components/SocialClubListingCard/socialClubListingCard.component.ts
+++ src/Components/SocialClubListingCard/socialClubListingCard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-social-club-listing-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './socialClubListingCard.component.html',
   styleUrl: './socialClubListingCard.component.css',
 })
