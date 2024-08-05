774a622884725bbed6761a1bdfb3f7a293ca4a10
SageBoots <u20456213@tuks.co.za>
2024-08-05 21:10:46 +0200

    Fixing Jest Test issue

--- src/oauth/oauth.component.spec.ts
+++ src/oauth/oauth.component.spec.ts
@@ -2,14 +2,27 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { OauthComponent } from './oauth.component';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mock-app-event',
+  template: '',
+})
+class MockEventComponent {}
+
+@Component({
+  template: '<app-mock-app-event></app-mock-app-event>',
+})
+class TestOauthComponent {}
 
 describe('OauthComponent', () => {
-  let component: OauthComponent;
+  let component: TestOauthComponent;
   let fixture: ComponentFixture<OauthComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [OauthComponent],
+      declarations: [TestOauthComponent, MockEventComponent],
       providers: [
         {
           provide: ActivatedRoute,
