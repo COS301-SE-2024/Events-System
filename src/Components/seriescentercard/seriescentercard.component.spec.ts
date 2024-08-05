5c2744f51ca5b2135895887439b14e7c3df04610
Mathenda <mathendam@gmail.com>
2024-08-05 21:47:52 +0200

    fixed failing test

--- src/Components/seriescentercard/seriescentercard.component.spec.ts
+++ src/Components/seriescentercard/seriescentercard.component.spec.ts
@@ -1,16 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { SeriescentercardComponent } from './seriescentercard.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { SeriescentercardComponent } from './seriescentercard.component'; //import oath -------------------------
 
-describe('SeriescentercardComponent', () => {
-  let component: SeriescentercardComponent;
-  let fixture: ComponentFixture<SeriescentercardComponent>;
+// Mock component
+@Component({
+  selector: 'app-mock-seriescentercard',
+  template: '',
+})
+class MockEventComponent {/*...*/}
+
+@Component({
+  template: '<app-mock-seriescentercard></app-mock-seriescentercard>', // Use the correct selector
+})
+class TestHostComponent {/*...*/}
+
+describe('EventComponent', () => {
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SeriescentercardComponent],
+      imports: [SeriescentercardComponent], // change to oauth  -----------------------
+      declarations: [TestHostComponent, MockEventComponent], // Remove EventComponent from declarations
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({ id: 'testId' }) // Add your own mock values here
+          }
+        }
+      ]
     }).compileComponents();
-
-    fixture = TestBed.createComponent(SeriescentercardComponent);
+  
+    fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
