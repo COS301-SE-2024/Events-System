3f1c0621b5212fc7ce057f6630e67b0455e9d17f
KezlynGovender <u22567993@tuks.co.za>
2024-08-08 14:34:08 +0200

    Added some functionality

--- src/host-checkin/host-checkin.component.ts
+++ src/host-checkin/host-checkin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
+
 @Component({
   selector: 'app-host-checkin',
   standalone: true,
@@ -9,7 +10,28 @@ import { RouterModule, Router } from '@angular/router';
   styleUrls: ['./host-checkin.component.css'],
 })
 
-export class HostCheckinComponent{
-  
-}
+export class HostCheckinComponent implements OnInit {
+  eventTitle = 'Event Name';
+  eventStarted = false;
+  attendeesCount = 0;
 
+  ngOnInit(): void {
+    // Initially fetch the number of attendees if the event has already started
+    this.fetchAttendeesCount();
+  }
+
+  checkInHost(): void {
+    this.eventStarted = true;
+    // Call the backend to indicate the event has started
+    // For now, we'll simulate the start
+    this.fetchAttendeesCount();
+  }
+
+  fetchAttendeesCount(): void {
+    if (this.eventStarted) {
+      // This would be a call to the backend to get the count of attendees
+      // For the sake of this example, we'll simulate it
+      this.attendeesCount = 50; // Replace with actual data fetching logic
+    }
+  }
+}
