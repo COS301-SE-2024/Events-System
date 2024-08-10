31ee0a1c38173d4155aa421ed88077b9ba96d8ee
Fentse <u21467422@tuks.co.za>
2024-08-10 14:52:59 +0200

    Modified social clubs styling

--- src/Components/SocialClubCard/socialClubCard.component.ts
+++ src/Components/SocialClubCard/socialClubCard.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { RandomImageServiceService } from '../../app/random-image-service.service'; 
 @Component({
   selector: 'app-social-club-card',
   standalone: true,
@@ -8,8 +9,21 @@ import { RouterModule } from '@angular/router';
   templateUrl: './socialClubCard.component.html',
   styleUrl: './socialClubCard.component.css',
 })
-export class SocialClubCardComponent {
+export class SocialClubCardComponent implements OnInit{
+
+  imageSource: string;
+  // Inject RandomImageService into the constructor
+  constructor(private randomImageService: RandomImageServiceService) {
+    this.imageSource = '';
+  }
+
+  ngOnInit(): void {
+    // Use the injected service
+    this.imageSource = this.randomImageService.getRandomImageSource();
+  }
+
   @Input() socialClubName: string | undefined;
   @Input() socialClubCreator: string | undefined;
   @Input() socialClubID: string | undefined;
+  @Input() socialClubDescription: string | undefined;
 }
