a20e06128e8abfb73970673b6c13d67bb6817a07
SageBoots <u20456213@tuks.co.za>
2024-08-11 20:56:44 +0200

    Completed e2e test for social clubs page

--- e2e/src/events.spec.ts
+++ e2e/src/events.spec.ts
@@ -3,22 +3,23 @@ import { test, expect } from '@playwright/test';
 // Constants for the test
 const BASE_URL = 'http://localhost:4200'; // Replace with your actual app URL
 
-// Sample cookie to simulate login
-const authCookie = {
-  name: 'jwt', // Adjust the name to match your app's cookie
-  value: 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0aG9tYXNkdWJvaXM0QGdtYWlsLmNvbSIsImlhdCI6MTcyMzMxODEyMiwiZXhwIjoxNzIzNDA0NTIyfQ.5ph1KdavNtFxxtMMJYEsz_Gz9Y2Il3NJ-mNggfM7KfQ', // Replace with a valid token or session ID
-  domain: 'localhost', // Adjust domain if needed
+// Sample cookies to simulate login
+const refreshTokenCookie = {
+  name: 'refresh',
+  value: 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0aG9tYXNkdWJvaXM0QGdtYWlsLmNvbSIsImlhdCI6MTcyMzMxODEyMiwiZXhwIjoxNzIzNDA0NTIyfQ.5ph1KdavNtFxxtMMJYEsz_Gz9Y2Il3NJ-mNggfM7KfQ', // Replace with the actual refresh token value
+  domain: 'localhost',
   path: '/',
   httpOnly: true,
-  secure: false, // Set to true if using HTTPS
+  secure: false,
   sameSite: 'Lax' as 'Lax',
+  // expires: new Date(new Date().getTime() + 24 * 60 * 60 * 1000).getTime(), // 24 hours
 };
 
 test.describe('EventsComponent Tests', () => {
   // Before all tests, navigate to the events page
   test.beforeEach(async ({ page }) => {
     // Set the authentication cookie before each test
-    await page.context().addCookies([authCookie]);
+    await page.context().addCookies([refreshTokenCookie]);
 
     // Navigate to a base page to access local storage
     await page.goto(`${BASE_URL}/events`, { waitUntil: 'domcontentloaded' }); // Ensure the document is fully loaded
@@ -38,7 +39,7 @@ test.describe('EventsComponent Tests', () => {
 
     // Check if the page loaded correctly
     // Check that the component and its elements are present
-    await expect(page.locator('app-events')).toBeVisible();;
+    await expect(page.locator('app-events')).toBeVisible();
 
     // Test filter options
       await page.click('button:has-text("Refine by social club")');
